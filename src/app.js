a204387da19bd082e2df93bcddc35ac909350bd7
atzleryuri <atzleryuri@gmail.com>
2022-06-22 23:40:30 -0300

    add-cors

--- src/app.js
+++ src/app.js
@@ -3,17 +3,18 @@ const express = require("express");
 const routes = require("./Routes");
 const appError = require("./Errors/appError");
 const uploadConfig = require("./config/multer");
+const cors = require("cors");
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  app.use(cors());
+  next();
+});
 app.use(express.json());
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "*");
-//   res.header("Access-Control-Allow-Headers", "*");
-//   app.use(cors());
-//   next();
-// });
 
 app.use("/images", express.static(uploadConfig.directory));
 
